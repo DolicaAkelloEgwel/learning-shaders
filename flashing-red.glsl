6d2d72e7c85413398f6af3221e10973f55e150e5
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-01-09 14:54:21 +0000

    using a function

--- flashing-red.glsl
+++ flashing-red.glsl
@@ -4,6 +4,10 @@ precision mediump float;
 
 uniform float u_time;
 
+vec4 red_blinking(float speed) {
+    return vec4(abs(sin(u_time * speed)),0.0,0.0,1.0);
+}
+
 void main() {
-	gl_FragColor = vec4(abs(sin(u_time)),0.0,0.0,1.0);
+    gl_FragColor = red_blinking(0.5);
 }
