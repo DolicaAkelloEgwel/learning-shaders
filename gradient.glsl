f69f1456aa5904ee3610bec847de1a8be8bf2a38
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-01-23 17:45:48 +0000

    attempting to normalise mouse

--- gradient.glsl
+++ gradient.glsl
@@ -10,5 +10,6 @@ uniform float u_time;
 
 void main() {
 	vec2 st = gl_FragCoord.xy / u_resolution;
-	gl_FragColor = vec4( (sin(st.x) * 0.5) + 0.5, (cos(st.y) * 0.5) + 0.5 ,abs(sin(u_time)),u_mouse.y / u_resolution.y);
+	vec2 m = u_mouse.xy / u_resolution;
+	gl_FragColor = vec4( (sin(st.x) * 0.5) + 0.5, (cos(st.y) * 0.5) + 0.5 ,abs(sin(u_time)),m.y);
 }
